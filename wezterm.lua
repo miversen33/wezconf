9336c0428f958901375f2935093060d67a8cd849
Mike Iversen <miversen33@gmail.com>
2023-06-10 18:44:00 -0500

    Migrated user configuration to true `user_conf.lua` file

--- wezterm.lua
+++ wezterm.lua
@@ -1,41 +1,8 @@
 local lib = require("lib")
-local wezterm = require("wezterm")
+local _, user_config = pcall(require, "user_wezconf")
 
+-- If you are looking for how to use this, check out the example found in `.wezterm.lua` in this directory or the `README.md`!
 
--- Your custom configuration. This will be merged with the default config.
--- This will take "precidence" over any options in the default configuration
--- Note, to "cancel" something in the default configuration, set its value to
--- "none"
--- EG: pref_fonts = "none"
-local user_config = {
-    pref_fonts = {'JetBrains Mono', 'MesloLGS NF'},
-    env_vars = { NOTMUX = "1" },
-    tab_bar_appearance = 'Retro',
-    tab_bar_location = 'Bottom',
-    cursor_style = 'BlinkingBar',
-    inactive_pane_hsb = {
-        saturation = 0.7,
-        brightness = 0.2
-    },
-    opacity = { window = 0.90 },
-    color_scheme = 'MaterialDesignColors',
-    colors = {
-        tab_bar = {
-            background = 'BLACK',
-            new_tab = {
-                bg_color = 'BLACK',
-                fg_color = 'WHITE'
-            },
-            new_tab_hover = {
-                bg_color = wezterm.color.parse('BLACK'):lighten(.40),
-                fg_color = 'WHITE',
-                italic = true
-            }
-        },
-    },
-    raw = {
-        window_close_confirmation = 'NeverPrompt',
-    }
-}
+user_config = _ == true and user_config or {}
 
 return lib.load(user_config)
